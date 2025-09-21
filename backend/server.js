d6b73ff1dd36c4e1fd1af83d743066c66bb048e2
mrsandy7702837779 <bobbysandy58@gmail.com>
2025-09-21 16:45:59 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,9 @@ const cors = require("cors");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-const BASE = process.env.BASE_URL || `http://localhost:${PORT}`;
+
+// Use your Render backend URL here
+const BASE = process.env.BASE_URL || `https://link-shortener-4ihq.onrender.com`;
 
 app.use(cors());
 app.use(express.json());
